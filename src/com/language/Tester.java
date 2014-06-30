bddef020820413c397053418371cab0354dba407
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-06-30 18:53:33 -0300

    Elimino espacios innecesarios

--- src/com/language/Tester.java
+++ src/com/language/Tester.java
@@ -6,14 +6,14 @@ import com.language.parser.ExpressionParser;
 public class Tester {
 
 	public static void main(String[] args) {
-		
+
 		String exptext = "19 * x + 3";
-		
+
 		Expression expobj = ExpressionParser.parse(exptext);
-		
+
 		System.out.println("Expresion obtenida: ");
 		System.out.println(expobj.toString());
-		
+
 	}
-	
+
 }
