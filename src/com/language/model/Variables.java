7b2ea39a88a6b2448d88e277db6bc1a4fe3d12e7
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-06-30 18:52:49 -0300

    Agrego clase singleton para variables

---
+++ src/com/language/model/Variables.java
@@ -0,0 +1,20 @@
+package com.language.model;
+
+// Clase Singleton que almacena las variables.
+public class Variables {
+	private static Variables INSTANCE = null;
+	 
+    private Variables() {}
+    
+    private synchronized static void createInstance() {
+    	if (INSTANCE == null) { 
+            INSTANCE = new Variables();
+        }
+    }
+	
+    public static Variables getInstance() {
+        if (INSTANCE == null) createInstance();
+        return INSTANCE;
+    }
+    
+}
