ad42edc133223ebd85d506f9f26f159c141c77fd
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-07-02 20:52:55 -0300

    Tester con nuevas funciones

--- src/com/language/Tester.java
+++ src/com/language/Tester.java
@@ -7,7 +7,9 @@ public class Tester {
 
 	public static void main(String[] args) {
 
-		String exptext = "var a = true; var b = 4;";
+		//String exptext = "while (a == b) { var a = true; var b = 4; var c = [1,2,3]; };";
+		String exptext = "for (var a = 1; a > 5; a++) { var a = true; var b = 4; var c = [1,2,3]; };";
+		
 		Expression expobj = ExpressionParser.parse(exptext);
 		
 		System.out.println("Parsing exitoso");
