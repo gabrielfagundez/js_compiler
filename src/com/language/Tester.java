f31277066a5e3336a0326f3bb6721db2afdb427d
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-07-03 20:46:31 -0300

    Agrego lectura de archivos en lugar de un string

--- src/com/language/Tester.java
+++ src/com/language/Tester.java
@@ -1,5 +1,7 @@
 package com.language;
 
+import java.io.*;
+
 import com.language.model.*;
 import com.language.parser.ExpressionParser;
 
@@ -7,13 +9,46 @@ public class Tester {
 
 	public static void main(String[] args) {
 
-		//String exptext = "while (a == b) { var a = true; var b = 4; var c = [1,2,3]; };";
-		String exptext = "function prueba1(){ var a = 3; }; function prueba2(){ var a = 3; }; prueba1();";
-		//String exptext = "var a = 3;";
+		String fileName = "tests/unit/syntax/test1.js";
+		String exptext = null;
+		BufferedReader br = null;
 
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e1) {
+			System.out.println("File not found. ");
+		}
+	    try {
+	        StringBuilder sb = new StringBuilder();
+	        String line = br.readLine();
+
+	        while (line != null) {
+	            sb.append(line);
+	            line = br.readLine();
+	            if(line != null) {
+	            	sb.append("\n");
+	            }
+	        }
+	        exptext = sb.toString();
+	    } catch (IOException e) {
+	    	System.out.println("An IO exception was raised. ");
+	    	e.printStackTrace();
+		} finally {
+	        try {
+				br.close();
+			} catch (IOException e) {
+				System.out.println("An IO exception was raised. ");
+				e.printStackTrace();
+			}
+	    };
+
+	    System.out.println("Parsing started..");
+	    System.out.println(exptext);
+	    System.out.println("");
+	    System.out.println("");
 		Expression expobj = ExpressionParser.parse(exptext);
 
-		System.out.println("Parsing exitoso");
+		//System.out.println("Parsing exitoso");
 
 	}
 
--- tests/unit/syntax/test1.js
+++ tests/unit/syntax/test1.js
@@ -1,4 +1,1 @@
 var aBa = 5;
-var aba = 3;
-console.log(aBa);
-console.log(aba);
