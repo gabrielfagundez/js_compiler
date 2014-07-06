8356c816b46365d20eb1dbdf57fda941cf2ccea2
Fabian Kremer <fkremer@MacBook-Pro-de-Fabian.local>
2014-07-06 15:55:05 -0300

    primera version de evaluacion de expresiones

--- src/com/language/model/Expression.java
+++ src/com/language/model/Expression.java
@@ -1,5 +1,6 @@
 package com.language.model;
 
+import java.lang.Math;
 public class Expression {
 
 	private Object value;
@@ -7,16 +8,34 @@ public class Expression {
 	private Expression right;
 	private String type;
 	
-	public Expression(Object value) {
-		this.value = value;
+	public Expression(Object value, String type) {
+		switch(type){
+			case "Boolean":
+				this.value =  new Boolean((String)value);
+				break;
+			case "Integer":
+				this.value =  new Integer((String)value);
+				break;
+			case "Float":
+				this.value =  new Float((String)value);
+				break;
+			case "String":	
+				this.value =  new String(((String)value).substring(1, ((String)value).length() - 1));
+				break;
+			default:
+				this.value =  new String((String)value);
+				break;
+		}
 		this.left = null;
 		this.right = null;
+		this.type = type;
 	}
 	
 	public Expression(Object value, Expression left, Expression right) {
 		this.value = value;
 		this.left = left;
 		this.right = right;
+		evaluateType();
 	}
 
 	public Object getValue() {
@@ -43,87 +62,215 @@ public class Expression {
 		this.right = right;
 	}
 	
-	public String evaluateType(){
-		return "string";
-	}
-
-	public Boolean isTruable(){
-		return true;
+	public String getType() {
+		return type;
 	}
 	
-	public Boolean isFalsable(){
-		return true;
-	}
-	
-	public String evaluate(){
-		
-		if(this.value == "&&"){
-			if(this.left.isTruable()){
-				return this.right.evaluate();
+	public void evaluateType(){
+		if(this.value=="+"){
+			if(this.left.type=="String" || this.right.type=="String"){
+				this.type = "String";
+			}else if (this.left.type=="Float" || this.right.type=="Float"){
+				this.type = "Float";
 			}else {
-				return this.right.evaluate();
+				this.type = "Integer";
 			}
-		}
-		else if(this.value == "||"){
-			if(this.left.isFalsable()){
-				return this.right.evaluate();
+		}else if ((this.value=="-") || (this.value=="*") || (this.value=="/")){
+			if (this.left.type=="String" || this.right.type=="String"){
+				this.type = "NaN";
+			}else if (this.left.type=="Float" || this.right.type=="Float"){
+				this.type = "Float";
 			}else{
-				return this.right.evaluate();
-			}			
-		}
-		
-		else if(this.value == "+"){
-			if (this.left.type == "String" || this.left.type == "String"){
-				return "Concatenate";
-			} else {
-				return "numerize";
+				this.type = "Integer";
 			}
-//			Integer int_value = new Integer(left.evaluate()) + new Integer(right.evaluate());
-//			return int_value.toString();
-		} else if (this.value == "+"){
-			return "gola";
+		}else if (this.value=="&&"){
+			if (this.left.isFalse()){
+				this.type = this.left.type;
+			}else{
+				this.type = this.right.type;
+			}			
+		}else if ((this.value=="||")){
+			if (this.left.isTrue()){
+				this.type = this.left.type;
+			}else{
+				this.type = this.right.type;
+			}			
+		}else if ((this.value=="==") || (this.value=="!=")|| (this.value=="<=") || 
+				(this.value==">=") || (this.value=="<") || (this.value==">")){
+			this.type = "Boolean";
+		}
+	}
+
+	public boolean isTrue(){
+		if (this.type=="Integer" || this.type == "Float"){
+			return (Integer)this.evaluate()>0;
+		} else if (this.type=="Boolean"){
+			return (Boolean)this.evaluate();
+		} else if (this.type=="String"){
+			return ((String)this.evaluate()).length()>0;
 		} else {
-			return (String)this.value;
+			return false;
 		}
 	}
 	
-	public String toString() {
-		return this.toString(0);
+	public boolean isFalse(){
+		return !isTrue();
 	}
-
-	public String toString(int level) {
-		StringBuffer sb = new StringBuffer();
-
-		String tab = "";
-		for (int i = 0; i < level; i++) {
-			tab += "  ";
+	
+	public double getArithmeticEvaluationForRamification(Expression exp){
+		double returnValue = 0;
+		switch(exp.type){
+			case "Boolean":
+				if ((Boolean)exp.evaluate()){
+					returnValue+=1;
+				}
+				break;
+			case "Integer":
+				returnValue = (Integer)exp.evaluate();
+				break;
+			case "Float":
+				returnValue = (Float)exp.evaluate();
+				break;
+			default:
+				returnValue = (Integer)exp.evaluate();
+				break;
 		}
-
-		sb.append(tab);
-		sb.append(this.value);
-
-		Expression l = this.left;
-		if(l != null){
-			sb.append("(\n");
-
-			String exptext = l.toString(level + 1);
-			sb.append(exptext);
-
-			Expression r = this.right;
-			if(r != null){
-				exptext = r.toString(level + 1);
-				sb.append(exptext);
-			} else {
-				sb.append("\n");
-			}
-
-			if(l != null){
-				sb.append(tab);
-				sb.append(")");
+		
+		return returnValue;
+	}
+	
+	public Object evaluateArithmetic(){
+		if(this.left.type=="String" || this.right.type=="String"){
+			if (this.value=="+"){
+				return this.left.evaluate().toString() + this.right.evaluate().toString();
+			}else{
+				return "NaN";	
 			}
 		}
+		else{
+			double left = getArithmeticEvaluationForRamification(this.left);
+			double right = getArithmeticEvaluationForRamification(this.right);
+			
+			double result = 0;
+			switch(this.value.toString()){
+				case "+":
+					result = left + right;
+					break;
+				case "-":
+					result = left - right;
+					break;
+				case "*":
+					result = left * right;
+					break;
+				default:
+					// /
+					try{
+						result = left / right;
+					}catch(ArithmeticException e){
+						//habria que hacer un mejor control de estos casos borde
+						result = 0;
+					}
+					break;
+			}
+			if (Math.floor(result)==(result)){
+				//la suma de doubles es .0, retorno un integer
+				return new Integer((int) (result));
+			}else
+				//la suma de doubles es .x con x>0 entonces retorno un float
+				return new Float(result);
+		}
+	}
+	
+	public double getComparisonEvaluationForRamification(Expression e){
+		double returnValue = 0;
+		switch(e.type){
+			case "Boolean":
+				if ((Boolean)e.evaluate()){
+					returnValue+=1;
+				}
+				break;
+			case "Integer":
+				returnValue = (Integer)e.evaluate();
+				break;
+			case "Float":
+				returnValue = (Float)e.evaluate();
+				break;
+			case "String":
+				try{
+					returnValue = new Integer((String)e.evaluate());
+				}catch (NumberFormatException exc){	
+					//habria que manejar mas este caso borde y devolver NaN
+					returnValue = 0;
+				}
+			default:
+				returnValue = (Integer)e.evaluate();
+				break;
+		}
+		
+		return returnValue;
+	}
+	
+	public Object evaluateComparison(){
 
-		sb.append("\n");
-		return sb.toString();
+		double left = getComparisonEvaluationForRamification(this.left);
+		double right = getComparisonEvaluationForRamification(this.right);
+			
+		switch(this.value.toString()){
+			case "==":
+				return left == right;
+			case "!=":
+				return left != right;
+			case "<=":
+				return left <= right;
+			case ">=":
+				return left >= right;
+			case ">":
+				return left > right;
+			default:
+				// case "<":
+				return left < right;
+		}
+		
+	}
+	
+	public Object evaluate(){
+		
+		switch(this.value.toString()){
+			case "&&":
+				if(this.left.isFalse()){
+					return this.left.evaluate();
+				}else {
+					return this.right.evaluate();
+				}
+			case "||":
+				if(this.left.isTrue()){
+					return this.left.evaluate();
+				}else{
+					return this.right.evaluate();
+				}	
+			case "==":
+				return evaluateComparison();
+			case "<=":
+				return evaluateComparison();
+			case ">=":
+				return evaluateComparison();
+			case "<":
+				return evaluateComparison();
+			case ">":
+				return evaluateComparison();
+			case "!=":
+				return evaluateComparison();
+			case "+":
+				return evaluateArithmetic();
+			case "-":
+				return evaluateArithmetic();
+			case "*":
+				return evaluateArithmetic();
+			case "/":
+				return evaluateArithmetic();
+			default:
+				//es un valor, no un simbolo
+				return this.value;
+		}
 	}
 }
---
+++ tests/unit/evaluation/test1.js
@@ -0,0 +1,22 @@
+var a1 = true && true;       // t && t returns true
+var a2 = true && false;      // t && f returns false
+var a3 = false && true;      // f && t returns false
+var a4 = false && (3 == 4);  // f && f returns false
+var a5 = "Cat" && "Dog";     // t && t returns Dog
+var a6 = false && "Cat";     // f && t returns false
+var a7 = "Cat" && false;     // t && f returns false
+
+var o1 = true || true;       // t || t returns true
+var o2 = false || true;      // f || t returns true
+var o3 = true || false ;     // t || f returns true
+var o4 = false || (3 == 4);  // f || f returns false
+var o5 = "Cat" || "Dog";     // t || t returns Cat
+var o6 = false || "Cat";     // f || t returns Cat
+var o7 = "Cat" || false;     // t || f returns Cat
+
+var c1 = 1 + 2; // 3
+var c6 =true + 1; // 2
+var c5 =false + false; // 0
+var c4 =5 + "foo" ;// "5foo"
+var c3 ="foo" + false; // "foofalse"
+var c2 ="foo" + "bar" ;// "foobar"
