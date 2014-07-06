aac433776553897273039ee3e34bda69952dec5e
Fabian Kremer <fkremer@MacBook-Pro-de-Fabian.local>
2014-07-06 15:56:01 -0300

    [Archivos Binarios] - Subo archivos actualizados

--- tests/unit/syntax/test1.js
+++ tests/unit/syntax/test1.js
@@ -1,15 +1,19 @@
-var aBa = 5;
-var aba = 5;
-
-function a(a, b, c) {
-    var test1 = a;
-    var test2 = b;
-    var suma = a + b + c;
+var aBa = 1  + false + 3.0;
+// var aba = 5;
+// // comment
+// function a(a, b, c) {
+// 	// hola
+//     var test1 = a;
+//     var test2 = b;
+//     var suma = a + b + c;
     
-    if(test1 > 0){
-        suma = suma - 1;
-    };
-}
+//     if(test1 > 0){
+//         suma = suma - 1;
+//     };
+//     /* this is a 
+//     three line
+//     comment*/
+// }
 
-a(1,2,3);
+// a(1,2,3);
     
