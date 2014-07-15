322225789152cc459c7a94b3b0eb2ae4aab72a55
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-07-15 19:30:35 -0300

    Agrego nuevo test con funciones y parametros

---
+++ tests/functional/auto.js
@@ -0,0 +1,26 @@
+// Funcion para mostrar como console.log
+function mostrame1p(nombreMostrar){
+	console.log(nombreMostrar);
+}
+mostrame1p('prueba');
+
+// Funcion para imprimir la suma
+function suma2(sum1, sum2){
+	console.log(sum1 + sum2);
+}
+suma2(1, 2);
+
+// Funcion para imprimir la suma de 3
+function suma3(sum1, sum2, sum3){
+	console.log(sum1 + sum2 + sum3);
+}
+suma3(1, 2, 3);
+
+// Funcion que RETORNA el producto
+function mult(a, b){
+	return a * b;
+}
+console.log(mult(4, 6));
+
+// Muchos concat juntos
+console.log("Auto ".concat("rojo, ").concat("lindo, ").concat("y chico."));
