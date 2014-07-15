edc815af360ffdc499105ab57955902981cbc926
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-07-15 18:11:44 -0300

    Cambios menores en un test

--- tests/functional/scopes.js
+++ tests/functional/scopes.js
@@ -1,25 +1,13 @@
 // Algo con array
-var prueba_41 = 1 + array_1;
 var prueba_42 = 1 + [1,2,3];
-var array =[1, "2", text];
+var array =[1, "2"];
 var bool_true = true;
 var bool_false = false;
 
-console.log(prueba_41);
 console.log(prueba_42);
 console.log(array);
 console.log(bool_true);
 console.log(bool_false);
-
-var numero_1;
-var numero_2;
-numero_1 = 3;
-numero_2 = 1;
-var resultado = numero_1 + numero_2;
-
-console.log(numero_1);
-console.log(numero_2);
-console.log(resultado);
 
 var numero_1 = 3;
 var numero_2 = 1;
@@ -53,80 +41,56 @@ console.log(b);
 }
 console.log(a);
 
-a = 1;
+var1 = 1;
 if (true) {
 	if (true) {
-		b = "hola";
-		console.log(a);
-		console.log(b);
+		var2 = "hola";
+		console.log(var1);
+		console.log(var2);
 	};
 };
-console.log(b);
+console.log(var2);
 if (true) {
 	a = 3;
-	console.log(a);
-	console.log(b);
+	console.log(var1);
+	console.log(var2);
 };
-console.log(a);
+console.log(var1);
 
 {
-	a = 1;
-	a = 2;
+	_var1 = 1;
+	_var2 = 2;
 }
-console.log(a);
+console.log(_var1);
 
-a = 1;
-{
-    {
-		b = "hola";
-		console.log(a);
-		console.log(b);
-	}
-	b = "ceci";
-}
-b = "lala";
-console.log(b);
-
-var a = "hola";
+var s1 = "hola";
 { 
-	var a = "pepe";
-	console.log(a);
+	var s1 = "pepe";
+	console.log(s1);
 }
-console.log(a);
+console.log(s1);
 
-var a = "pepe";
+var s2 = "pepe";
 if (true) {
-	var a = 9879;
-	console.log(a);
+	var s2 = 9879;
+	console.log(s2);
 }
 
-var a = "pepe";
-for (var i = 0; i <= 1; i++) {
- var a = "hola";
- console.log(a);
-};
+var asd = "pepe";
+var jsd = 1;
 
-for(var i=0;i<4;i++){
-	console.log(i);
-}
-
-for(var i=0;i<4;i++){
-	console.log(i);
-}
-var j = 1;
-
-a = 1;
+asd = 1;
 {
 	{
-		b = "hola";
-		console.log(a);
-		console.log(b);
+		bsd = "hola";
+		console.log(asd);
+		console.log(bsd);
 	}
 }
-console.log(b);
+console.log(bsd);
 {
 	a = 3;
-	console.log(a);
-	console.log(b);
+	console.log(asd);
+	console.log(bsd);
 }
-console.log(a);
+console.log(asd);
